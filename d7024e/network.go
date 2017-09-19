a83e82dbc364c070dd0979ec6e2cdf5d50346a37
Unknown <joakim.karl.larsson@gmail.com>
2017-09-19 15:23:59 +0200

    Work on contact pinging eachother

--- d7024e/network.go
+++ d7024e/network.go
@@ -1,57 +1,53 @@
 package d7024e
 
 import (
-    "fmt"
-    "net"
-    "strconv"
-    //"D7024E-Kademlia/protobuf"
-    "github.com/golang/protobuf/proto"
+	"fmt"
+	"net"
+	"strconv"
+	//"D7024E-Kademlia/protobuf"
+	"github.com/golang/protobuf/proto"
 )
 
 type Network struct {
-
+	me Contact
 }
 
 func Listen(ip string, port int) {
-	Addr,err1 := net.ResolveUDPAddr("udp",ip+":"+strconv.Itoa(port))
+	Addr, err1 := net.ResolveUDPAddr("udp", ip+":"+strconv.Itoa(port))
 	Conn, err2 := net.ListenUDP("udp", Addr)
-  if (err1  != nil) || (err2 != nil){
-      fmt.Println("Connection Error: ", err1,"\n", err2)
-  }
+	if (err1 != nil) || (err2 != nil) {
+		fmt.Println("Connection Error: ", err1, "\n", err2)
+	}
 
 	//read connection
 	defer Conn.Close()
 	buf := make([]byte, 1024)
 	for {
-			_,_,err := Conn.ReadFromUDP(buf)
-      handleMessage(buf)
-			if err != nil {
-					fmt.Println("Read Error: ",err)
-			}
+		_, _, err := Conn.ReadFromUDP(buf)
+		go handleMessage(buf, ip+":"+strconv.Itoa(port))
+		if err != nil {
+			fmt.Println("Read Error: ", err)
+		}
 	}
 }
 
-
 func (network *Network) SendPingMessage(contact *Contact) {
-  message := buildMessage("ping")
-  data, err := proto.Marshal(message)
-  if (err  != nil)  {
-      fmt.Println("Marshal Error: ", err)
-  }
+	message := buildMessage("ping", contact.ID.String(), contact.Address)
+	data, err := proto.Marshal(message)
+	if err != nil {
+		fmt.Println("Marshal Error: ", err)
+	}
 	Conn, err := net.Dial("udp", contact.Address)
-  if (err  != nil)  {
-      fmt.Println("UDP-Error: ", err)
-  }
-  defer Conn.Close()
+	if err != nil {
+		fmt.Println("UDP-Error: ", err)
+	}
+	defer Conn.Close()
 
-  _,err = Conn.Write(data)
-  if err  != nil {
-      fmt.Println("Write Error: ", err)
-  }
+	_, err = Conn.Write(data)
+	if err != nil {
+		fmt.Println("Write Error: ", err)
+	}
 }
-
-
-
 
 func (network *Network) SendFindContactMessage(contact *Contact) {
 	// TODO
--- main.go
+++ main.go
@@ -1,26 +1,30 @@
 package main
 
 import (
-  kademlia "D7024E-Kademlia/d7024e"
-  "sync"
-  //"fmt"
-
-  //"log"
-  //"github.com/golang/protobuf/proto"
-  //"D7024E-Kademlia/protobuf"
+	kademlia "D7024E-Kademlia/d7024e"
+	"sync"
+	//"fmt"
 )
 
-func main () {
-  var mutex = &sync.Mutex{}
-  mutex.Lock()
-  defer mutex.Unlock()
-  contact := kademlia.NewContact(kademlia.NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "127.0.0.1:8001")
-  //contact2 := kademlia.NewContact(kademlia.NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "127.0.0.1:8002")
+//export GOPATH=$HOME/go
+func main() {
+	var mutex = &sync.Mutex{}
+	mutex.Lock()
+	defer mutex.Unlock()
 
-  net := kademlia.Network{}
-  go net.SendPingMessage(&contact)
-  //go net.SendPingMessage(&contact)
-  //go net.SendPingMessage(&contact2)
-  kademlia.Listen("127.0.0.1", 8001)
+	contact := kademlia.NewContact(kademlia.NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "127.0.0.1:8001")
+	net := kademlia.Network{this}
+	go net.SendPingMessage(&contact)
 
+	kademlia.Listen("127.0.0.1", 8002)
+	/*
+		rt := kademlia.NewRoutingTable(kademlia.NewContact(kademlia.NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000"))
+		for i := 1; i < 10; i++ {
+			rt.AddContact(kademlia.NewContact(kademlia.NewRandomKademliaID(), "localhost:800"+strconv.Itoa(i)))
+		}
+		contacts := rt.FindClosestContacts(kademlia.NewKademliaID("FFFFFFFF00000000000000000000000000000000"), 20)
+		for i := range contacts {
+			fmt.Println(contacts[i].String())
+		}
+	*/
 }
