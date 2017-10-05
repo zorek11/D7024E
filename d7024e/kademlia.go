a819d6592cc736c7fc8026bcd7c1a78e6410a28e
Korre <alex.david.lofstrand@hotmail.com>
2017-10-05 14:19:12 +0200

    removed bad code

--- d7024e/kademlia.go
+++ d7024e/kademlia.go
@@ -84,77 +84,61 @@ func (kademlia *Kademlia) LookupContact(target *Contact) {
 
 		}
 	}
-	/*
-		for {
-			for k := 0; k < thisalpha; k++ {
-
-				if networks[k].GetResponse() != nil {
-					fmt.Println(networks[k].GetResponse()[0].String())
-					break
-				}
-
-				//fmt.Println(networks[k].GetResponse())
-				//fmt.Println(networks[k].response == nil)
-				if networks[k].GetResponse() != nil {
-					if networks[k].response[0].ID == target.ID {
-						fmt.Println("Target found: " + target.String())
-						fmt.Println("With address: " + networks[k].response[0].String())
-						return
-						//return networks[k].response[0]
-					} else {
-						tempAlpha := alpha % (len(networks[k].response) + 1)
-						tempNetworks := getNewNetworks(kademlia, networks[k].GetResponse(), tempAlpha, target)
-						sendFindContactForAll(tempNetworks, target)
-						networks = remove(networks, k)
-						networks = combineNetworks(networks, tempNetworks)
-						thisalpha = len(networks)
-
-					}
-				}
-
-			}
-		}
-	*/
-	//return &contacts[0]
 
 }
 
 func (kademlia *Kademlia) LookupData(hash string) {
+
+	target := NewContact(NewKademliaID(hash), "")
+	contacts := kademlia.nt.rt.FindClosestContacts(target.ID, count)
+	//thisalpha := alpha % (len(contacts) + 1)
+	fmt.Println(len(contacts))
+	if contacts[0].ID == target.ID {
+		//return &contacts[0]
+		fmt.Println("Target found: " + target.String())
+		fmt.Println("With address: " + contacts[0].String())
+		return
+	}
+
+	//tempnetwork := NewNetwork(nt.rt, kademlia)
+	kademlia.nt.AddMessage(&target)
+
+	go kademlia.nt.SendFindContactMessage(&contacts[0])
+	//}
+	x := 0
+	for {
+		if x < len(kademlia.GetNetwork().GetResponse()) {
+			//fmt.Println("Response in kademlia: ", kademlia.GetNetwork().GetResponse())
+			//if kademlia.GetNetwork().GetResponse()[0] != nil {
+			temp := kademlia.GetNetwork().GetResponse()[x]
+			x++
+			if temp[0].ID.String() == target.ID.String() {
+				fmt.Println("This is the correct ID String: " + temp[0].ID.String())
+				kademlia.found = true
+				return
+			} else {
+				var mutex = &sync.Mutex{}
+				mutex.Lock()
+				for i := 0; i < alpha; i++ {
+					if i >= len(temp) {
+						break
+					}
+					//fmt.Println("This is the new: ", temp[i])
+					go kademlia.nt.SendFindContactMessage(&temp[i])
+
+				}
+
+				//kademlia.nt.RemoveFirstResponse()
+				mutex.Unlock()
+
+			}
+			//}
+
+		}
+	}
+
 }
 
 func (kademlia *Kademlia) Store(data []byte) {
 	// TODO
-}
-
-/*
-func getNewNetworks(kademlia *Kademlia, contacts []Contact, alpha int, target *Contact) []*Network {
-	networks := make([]*Network, alpha)
-
-	for i := 0; i < alpha; i++ {
-		networks[i] = NewNetwork(kademlia.rt.me, kademlia)
-		networks[i].AddMessage(target)
-	}
-	return networks
-}
-*/
-func sendFindContactForAll(networks []*Network, target *Contact) {
-	for i := 0; i < len(networks); i++ {
-		go networks[i].SendFindContactMessage(target)
-	}
-}
-
-func remove(networks []*Network, s int) []*Network {
-	return append(networks[:s], networks[s+1:]...)
-}
-
-func combineNetworks(first []*Network, second []*Network) []*Network {
-	networks := make([]*Network, (len(second) + len(first)))
-	for i := 0; i < len(first); i++ {
-		networks[i] = first[i]
-	}
-	for j := len(first) - 1; j < len(second); j++ {
-		networks[j] = first[j-len(first)+1]
-	}
-	return networks
-
 }
